d051d538842626448870a75183bfeb01306db207
Ana Colautti <ana@easytech.com.ar>
2022-10-28 12:44:10 -0300

    agrego links relativos - prueba

--- src/sections/footer.php
+++ src/sections/footer.php
@@ -3,7 +3,8 @@
 function footer() {
     print('<button onclick="topFunction()" id="btn-scroll-top" title="Volver al inicio de la página">Subir</button>');
     print ('<div class="footer"><div>');
-    print ('<p class="center">Gracias!</p>');
-    print ('<p class="center">Ana, Nere y Mau</p>');
+//    print ('<p class="center">Gracias!</p>');
+//    print ('<p class="center">Ana, Nere y Mau</p>');
+    print ('<p class="center">Volver al <a href="/docs/sitemap.html">índice</a></p>');
     print ('</div></div>');
 }
